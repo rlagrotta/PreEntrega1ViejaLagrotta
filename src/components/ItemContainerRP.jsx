69b3099239e2ab2189d592bc132d09a41714942b
rlagrotta <roque@rocketpowerstudios.com>
2024-10-17 22:12:01 -0500

    last

--- src/components/ItemContainerRP.jsx
+++ src/components/ItemContainerRP.jsx
@@ -3,6 +3,11 @@ import React, { useEffect, useState } from 'react';
 const ItemContainerRP = ({ category }) => {
   const [data, setData] = useState([]);
 
+  function handleLinks(){
+
+  }
+
+  
   useEffect(() => {
     if (category) {
       fetch(`https://fakestoreapi.com/products/category/${category}`)
@@ -26,16 +31,30 @@ const ItemContainerRP = ({ category }) => {
   );
 
   const listItems = filteredData.map(item => (
-    <li key={item.id}>
-      {item.title} - ${item.price}
-    </li>
+    <div className="card" key={item.id} style={{"width": "18rem"}}>
+      <img 
+        className="card-img-top" 
+        src={item.image} 
+        alt="Card image cap" 
+        style={{"width": "100%", "height": "200px", "objectFit": "cover"}} // Asegura que todas las imágenes tengan el mismo tamaño
+      />
+      <div className="card-body">
+        <h5 className="card-title">{item.title}</h5>
+        <p className="card-text">{item.description}</p>
+        <a href="#" className="btn btn-primary">See more</a>
+      </div>
+    </div>
   ));
 
   return (
-    <div className='row-wrapper'>
-      <ul>
-        {listItems.length > 0 &&{data} ? listItems : {data}? <li>Cargando Items</li> : <li>No hay productos disponibles en esta categoría.</li>}
-      </ul>
+    <div className='row-wrapper mt-3 justify-content-center d-flex'>
+      <div className='row' style={{"justify-content": "space-evenly", "width":"90%"}}>
+        {listItems.length > 0 
+          ? listItems 
+          : data.length === 0 
+            ? <li>Cargando Items</li> 
+            : <li>No hay productos disponibles en esta categoría.</li>}
+      </div>
     </div>
   );
 }
